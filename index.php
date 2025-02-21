57328c7a2a10784c6441c3f74de01067b1de1a68
tanoudiallo <tanoudialo04@gmail.com>
2025-02-21 18:38:22 +0100

    modification du fichier index pour inclure les differentes sections du projets

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@3.0.0/fonts/remixicon.css"> <!-- Bibliotheque css pour nav__close et nav__tooggle -->
+    <link rel="stylesheet" href="styles.css">
+    <title>my_portfolio</title>
+</head>
+
+<body>
+<!--HEADER-->
+    <header class="header" id="header">
+        
+        <?php include 'pages/menu_nav.html'; ?>
+
+    </header>
+<!-- MAIN -->
+    <main class="main">
+        <!-- Home -->
+        <?php include 'pages/home.html'; ?>
+
+        <!-- About -->
+        <?php include 'pages/about.html'; ?>
+
+        <!-- Competences -->
+        <?php include 'pages/competences.html'; ?>
+
+        <!-- Service -->  <!--Je peux remplacer cette section par experiences-->
+        <?php include 'pages/experiences.html'; ?>
+
+        <!-- Projets -->
+        <?php include 'pages/projets.html'; ?>
+
+        <!-- Contact -->
+        <?php include 'pages/contact.html'; ?>
+
+    </main>
+
+<!-- FOOTER -->
+    <footer class="footer">
+    <?php include 'pages/footer.html'; ?>
+    </footer>
+
+    <!-- Defilement vers le haut -->
+     <a href="#" class="scrollup" id="scroll-up">
+        <i class="ri-arrow-up-line"></i>
+     </a>
+
+    <!-- EMAIL JS : a changer plus tard pour utiliser du php -->
+    <script src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js"></script>
+
+    <!-- ScrollReveal JS -->
+    <script src="scrollreveal.min.js"></script>
+  
+    <!-- MAIN JS -->
+    <script src="main.js" ></script>
+
+</body>
+</html>
