a09fc58f2fddad8c75d093ddf21477b565eddebb
tanoudiallo <tanoudialo04@gmail.com>
2025-03-18 01:36:14 +0100

    modification des inclusion php

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <?php 
+    header('Content-Type: text/html; charset=UTF-8');
+    ?>
+
+    <?php 
+    $pages_path = "pages/";
+    ?>
+
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@3.0.0/fonts/remixicon.css"> <!-- Bibliotheque css pour nav__close et nav__tooggle -->
     <!-- import des fichier css -->
-    <link rel="stylesheet" href="assets/css/commun.css">
-    <link rel="stylesheet" href="assets/css/a_propos.css">
-    <link rel="stylesheet" href="assets/css/competences.css">
-    <link rel="stylesheet" href="assets/css/contact.css">
-    <link rel="stylesheet" href="assets/css/experiences.css">
-    <link rel="stylesheet" href="assets/css/footer.css">
-    <link rel="stylesheet" href="assets/css/home.css">
-    <link rel="stylesheet" href="assets/css/projets.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
 
     <title>my_portfolio</title>
 </head>
@@ -20,35 +21,25 @@
 <body>
 <!--HEADER-->
     <header class="header" id="header">
-        
-        <?php include 'pages/menu_nav.html'; ?>
-
+        <?php include_once $pages_path . 'menu_nav.html'; ?>
     </header>
+
 <!-- MAIN -->
     <main class="main">
-        <!-- Home -->
-        <?php include 'pages/home.html'; ?>
-
-        <!-- About -->
-        <?php include 'pages/about.html'; ?>
-
-        <!-- Competences -->
-        <?php include 'pages/competences.html'; ?>
-
-        <!-- Service -->  <!--Je peux remplacer cette section par experiences-->
-        <?php include 'pages/experiences.html'; ?>
-
-        <!-- Projets -->
-        <?php include 'pages/projets.html'; ?>
-
-        <!-- Contact -->
-        <?php include 'pages/contact.html'; ?>
-
+        <?php 
+        include_once $pages_path . 'home.html'; 
+        include_once $pages_path . 'about.html'; 
+        include_once $pages_path . 'competences.html'; 
+        include_once $pages_path . 'experiences.html'; 
+        include_once $pages_path . 'projets.html'; 
+        include_once $pages_path . 'contact.html'; 
+        ?>
+    
     </main>
 
 <!-- FOOTER -->
     <footer class="footer">
-    <?php include 'pages/footer.html'; ?>
+        <?php include_once $pages_path . 'footer.html'; ?>
     </footer>
 
     <!-- Defilement vers le haut -->
@@ -60,10 +51,10 @@
     <script src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js"></script>
 
     <!-- ScrollReveal JS -->
-    <script src="assets/js/scrollreveal.min.js"></script>
+    <script src="assets/js/scrollreveal.min.js" defer></script>
   
     <!-- MAIN JS -->
-    <script src="assets/js/main.js" ></script>
+    <script src="assets/js/main.js" defer></script>
 
 </body>
 </html>
