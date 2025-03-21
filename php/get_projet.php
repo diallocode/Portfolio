386d4fc8cbf5e7b772881e83792472a3d76dc227
tanoudiallo <tanoudialo04@gmail.com>
2025-03-21 23:19:03 +0100

    fichier supprimer

--- php/get_projet.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'Database.php';
-$db = Database::getInstance()->getConnection();
-
-$stmt = $db->query("SELECT * FROM projects");
-$projects = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-header('Content-Type: application/json');
-echo json_encode($projects);
