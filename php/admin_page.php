331c893a1c1210da62ca65e7417bfdf3c3a32635
tanoudiallo <tanoudialo04@gmail.com>
2025-03-21 23:13:59 +0100

    ajout du sript php pour gerer l'acces via la page de login

--- php/admin_page.php
+++ php/admin_page.php
@@ -1,4 +1,11 @@
 <?php 
+session_start();
+
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+    header('Location: login.php');
+    exit;
+}
+
 require_once 'Database.php';
 $db = Database::getInstance()->getConnection();
 
